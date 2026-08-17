e931f411ea02f180c13c1a7be8143554fe4de6a9
Matteo Curcio <hello@matteocurcio.com>
2026-08-17 21:23:52 +1000

    Add Musetta album and sync reel artwork

---
+++ src/data/musicAlbumLinks.ts
@@ -0,0 +1,11 @@
+export const MUSIC_ALBUM_LINKS: Record<string, string> = {
+  "/assets/reels/reels-music/album_ultramarine-remixes.jpg": "https://musetta.bandcamp.com/album/ultramarine-the-remixes",
+  "/assets/reels/reels-music/album_atlantide.jpg": "https://musetta.bandcamp.com/album/atlantide",
+  "/assets/reels/reels-music/album_telescope.jpg": "https://musetta.bandcamp.com/album/telescope",
+  "/assets/reels/reels-music/album_ultramarine.jpg": "https://musetta.bandcamp.com/album/ultramarine",
+  "/assets/reels/reels-music/album_mice-to-meet-you.jpg": "https://musetta.bandcamp.com/album/mice-to-meet-you",
+  "/assets/reels/reels-music/album_red-star-ep.jpg": "https://musetta.bandcamp.com/album/red-star-ep",
+  "/assets/reels/reels-music/album_peace-and-melody-ep.jpg": "https://musetta.bandcamp.com/album/peace-and-melody-ep",
+  "/assets/reels/reels-music/album_nicotine-ep.jpg": "https://musetta.bandcamp.com/album/nicotine-ep",
+  "/assets/reels/reels-music/album_standing-by-my-side-ep.jpg": "https://musetta.bandcamp.com/album/standing-by-my-side-ep"
+};
